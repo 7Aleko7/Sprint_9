bd7e62ef2a7b17bc9fff17f02c0ecef04a5432e7
Aleksandr Ivanov <g.ialexp@yandex.ru>
2025-04-09 22:51:54 +0300

    change data

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import random
 import string
 from pathlib import Path
+import os
 
 class Data:
     @staticmethod
@@ -9,14 +10,11 @@ class Data:
         random_string = ''.join(random.choice(letters) for i in range(length))
         return random_string
 
-    home =Path.home()
-    cookie_photo = Path(home, 'PycharmProjects', 'Sprint_9','торт.jpg')
-
     COOKIE_RECIPE = {
         'name': 'Торт',
         'ingredient': 'шоколад белый',
         'weight': '500',
         'time': '3',
         'description': 'Только шоколад',
-        'photo': str(cookie_photo)
+        'photo': os.path.join(os.path.dirname(__file__), 'test_data', 'торт.jpg')
     }
